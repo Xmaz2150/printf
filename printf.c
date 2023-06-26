2ef777c4a77eeff9265aae7641a8bdc2c18f5dfd
taf <muzirat0@gmail.com>
2023-06-26 15:25:13 +0000

    printf v2

--- printf.c
+++ printf.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
+int is_match(const char* s, const char c)
 {
 	int i;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if(s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+int _printf(const char *format, ...)
+{
+	int i, len;
 	va_list ap;
+	const char* c = "cs";
 
 	va_start(ap, format);
 	i = 0;
+	len = 0;
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
@@ -14,16 +29,25 @@ int _printf(const char *format, ...)
 			switch (format[i + 1])
 			{
 				case 'c':
-					i += _char((char)va_arg(ap, int)) + 1;
+					i += 2;
+					len += _char((char)va_arg(ap, int));
 					break;
 				case 's':
-					i += _str(va_arg(ap, char*));
+					i += 2;
+					len +=  _str(va_arg(ap, char*));
+					break;
+				case '%':
+					i += 2;
+				 	len += _char('%');
+					if (is_match(c, format[i + 1]))
+							_char(format[i + 1]);
 					break;
 			}
 		}
 		_putchar(format[i]);
+		len++;
 		i++;
 	}
-	printf("i:%d", i);
+	printf("\ni:%d\n", len);
 	return (i);
 }
